3f4242bf8f8744420084b8fa68f3232bedaab87d
Karam <karamchandr@users.noreply.github.com>
2018-11-25 10:12:15 +0530

    Added file - take.php
    Code to upload files to a specified location and give a response "Success" if it was successful

---
+++ take.php
@@ -0,0 +1,27 @@
+<?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"uploaded/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+<html>
+   <body>
+      
+      <form action="" method="POST" enctype="multipart/form-data">
+         <input type="file" name="image" />
+         <input type="submit"/>
+      </form>
+      
+   </body>
+</html>
